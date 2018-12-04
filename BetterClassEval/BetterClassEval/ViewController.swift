e7e0a5494f8d8b0e41a7f090d7cbe8a971790b17
Rico Wang <ricowang@uw.edu>
2018-12-03 17:26:44 -0800

    So viewDidLoad() does load multiple times

--- BetterClassEval/BetterClassEval/ViewController.swift
+++ BetterClassEval/BetterClassEval/ViewController.swift
@@ -18,145 +18,21 @@ class ViewController: UIViewController, WKUIDelegate {
 
         super.viewDidLoad()
 
-//        writeLogToDisk(fileName: "log")
+        writeLogToDisk(fileName: "log")
 
         let user = Authentication(username: Creds().username, password: Creds().password)
-        self.doEverything(user)
+        let URLList: [String] = Request().readLocalURL()
 
+        requestEvalFromList(user: user, urlList: URLList, completion: { result in
+            NSLog(result.debugDescription)
+        })
 
-        // TODO: probably needs a DispatchQueue here?
-//        getEvalList(urlList: urlList, completion: { result in
-//            NSLog("getting resultlist back")
-//            NSLog(result.debugDescription)
-//        })
-
-        // Manually getting local data
-//        let chars: [Character] = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p",
-//                                  "q", "r", "s", "t", "u", "v", "w"]
-//        var urlList: [String] = []
-//        for char in chars {
-//            urlList.append("http://localhost:8123/" + String(char) + "-toc.html")
-//        }
-
-        // Getting all class URLs from ToC pages
-//        for url in urlList {
-//            HTMLParser().getAllClasses(url, completion: { result in
-//                let hrefs: [[String : String]] = result
-//
-//                for href in hrefs {
-//                    let link: String = href["link"]!
-//                    NSLog(link)
-        //// Getting course pages
-////                user.getCoursePageWithCookie(link, completion: { result in
-////                    HTMLParser().getStatsFromPage(result, completion: { result in
-////                        NSLog(result.debugDescription) })
-////                })
-//                }
-//
-//            })
-//        }
+        // https://stackoverflow.com/questions/1081131/viewdidload-getting-called-twice-on-rootviewcontroller-at-launch
+        // So this is expected to be executed twice.
     }
 
-    func doEverything(_ user: Authentication) {
 
-        // TODO: still gets done twice
-//        user.webLoginFirstKiss(completion: {
-//            user.printFields()
-//        })
 
-        // Target URL list
-        var urlList: [String] = []
 
-        // test data
-//        urlList = ["https://www.washington.edu/cec/a/AA101A2098.html",
-//                   "https://www.washington.edu/cec/a/AA198A1001.html",
-//                   "https://www.washington.edu/cec/a/AA210A1861.html",
-//                   "https://www.washington.edu/cec/a/AA210A2099.html"]
-
-        // Load local URL list
-        if let path = Bundle.main.path(forResource: "20", ofType: "txt") {
-            do {
-                let content = try String(contentsOfFile: path, encoding: .utf8)
-                urlList = content.components(separatedBy: .newlines)
-            } catch {
-                NSLog("Unspecified error")
-            }
-        }
-
-        for url in urlList {
-            NSLog(url)
-        }
-
-        getEvalList(user: user, urlList: urlList, completion: { _ in })
-    }
-
-    func getEvalList(user: Authentication, urlList: [String], completion: @escaping ([[String: Any]]) -> Void) {
-
-        /*
-        To any poor souls whom try to maintain this spaghetti, you have my warning - don't.
-
-        Just use it.
-
-        This service literally retires on Dec. 30, 2018 and I am not intended to maintain it unless they switch to
-        oauth or something similar.
-
-        The complete flow of requesting a review page is as followed:
-        1. Issue a GET request to UW Weblogin, get the hidden fields
-        2. POST user creds and the hidden fields from Step 1 to UW Weblogin to *actually* log in,
-           record pubcookie_l from response
-        3. GET the review page, record pubcookie_g_req and relay_url from response
-        4. POST to UW Weblogin with [Cookie: pubcookie_l] as header, and pubcookie_g_req=?&relay_url=? as body,
-           record pubcookie_g from the hidden fields from response
-        5. GET the review page with [Cookie: pubcookie_g] as header, and parse the resulting HTML
-
-        TODO: Will simplify multiple requests and release later so no need to do all five steps for every request.
-              So long as cookies are valid, the pubcookie_g is the universal key to any review page
-        */
-
-        // TODO: construct (serialize?) evaluation statistics (class Stats: Equatable, Hashable)
-//        var evalList: Set = Set.init()
-
-        var resultList: [[String: Any]] = []
-
-        // Simplified flow
-//        for url in urlList {
-//            // If cookies are not valid, do everything
-//            if !user.cookiesAreValid() {
-//                // Step 1
-//                user.webLoginFirstKiss(completion: {
-//                    // Step 2
-//                    user.weblogin(completion: {
-//                        // Step 3
-//                        user.getCoursePage(url, completion: {
-//                            // Step 4, gets pubcookie_g
-//                            user.webloginRedirect(url, completion: {})})})})
-//            }
-//
-//            // Step 5: Do whatever with pubcookie_g
-//            user.getCoursePageWithCookie(url, completion: { result in
-//                HTMLParser().getStatsFromPage(result, completion: { result in
-//                    NSLog(result.debugDescription)
-//                    resultList.append(result)})})
-
-        // Does everything for every quest
-        for url in urlList {
-            // Step 1
-            user.webLoginFirstKiss(completion: {
-                // Step 2
-                user.weblogin(completion: {
-                    // Step 3
-                    user.getCoursePage(url, completion: {
-                        // Step 4, gets pubcookie_g
-                        user.webloginRedirect(url, completion: {
-                            user.getCoursePageWithCookie(url, completion: { result in
-                                HTMLParser().getStatsFromPage(result, completion: { result in
-                                    NSLog(result.debugDescription)
-                                    resultList.append(result) })})})})})})
-        }
-
-        // TODO: fix completing prematurely
-//        NSLog(resultList.debugDescription)
-        completion(resultList)
-    }
 
 }
