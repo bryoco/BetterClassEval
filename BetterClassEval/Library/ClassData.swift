a76640e5ab2540228dc947a1f30430a7343ee297
Rico Wang <ricowang@uw.edu>
2018-12-08 16:41:21 -0800

    Upload evals from user

--- BetterClassEval/Library/ClassData.swift
+++ BetterClassEval/Library/ClassData.swift
@@ -31,14 +31,43 @@ struct ClassData: CustomDebugStringConvertible {
 
         self.debugDescription =
                 "key = \(self.key)\n" +
-                "class = \(self.className)\n" +
-                "lecturer = \(self.lecturer)\n" +
-                "quarter = \(self.quarter)\n" +
-                "enrolled = \(String(self.enrolled))\n" +
-                "surveyed = \(String(self.surveyed))\n" +
-                "statistics = \(self.statistics.debugDescription)"
+                        "class = \(self.className)\n" +
+                        "lecturer = \(self.lecturer)\n" +
+                        "quarter = \(self.quarter)\n" +
+                        "enrolled = \(String(self.enrolled))\n" +
+                        "surveyed = \(String(self.surveyed))\n" +
+                        "statistics = \(self.statistics.debugDescription)"
 
 //        NSLog("New classData created:\n\(self.debugDescription)")
+    }
+
+    init(_ data: [String : Any]) {
+        self.key = ""
+
+        self.className = data["class"] as! String
+        self.lecturer = data["lecturer"] as! String
+        self.quarter = data["quarter"] as! String
+        self.enrolled = Int(data["enrolled"] as! String)!
+        self.surveyed = Int(data["surveyed"] as! String)!
+        self.statistics = data["statistics"] as! [String: [String]]
+
+        self.debugDescription =
+                "key = \(self.key)\n" +
+                        "class = \(self.className)\n" +
+                        "lecturer = \(self.lecturer)\n" +
+                        "quarter = \(self.quarter)\n" +
+                        "enrolled = \(String(self.enrolled))\n" +
+                        "surveyed = \(String(self.surveyed))\n" +
+                        "statistics = \(self.statistics.debugDescription)"
+    }
+
+    func getSelf() -> [String : Any] {
+        return ["class": self.className,
+                "lecturer": self.lecturer,
+                "quarter": self.quarter,
+                "enrolled": self.enrolled,
+                "surveyed": self.surveyed,
+                "statistics": self.statistics]
     }
 
 }
--- BetterClassEval/Library/QueryFirebase.swift
+++ BetterClassEval/Library/QueryFirebase.swift
@@ -14,52 +14,76 @@ class QueryFirebase {
         self.ref = Database.database().reference()
     }
 
-    public func queryByLecturer(_ lecturer: String, completion: @escaping (([ClassData]) -> ())) {
+    public func queryByLecturer(target: String, lecturer: String, completion: @escaping (([ClassData]) -> ())) {
 
-        let q = ref.child("UW")
+        if target != "UW" && target != "userEval" {
+            NSLog("Invalid target")
+            return
+        }
+
+        let q = self.ref.child(target)
                 .queryOrdered(byChild: "lecturer")
                 .queryEqual(toValue: lecturer)
 
         self.query(q, completion: { result in completion(result) })
-
-//        var results: [classData] = []
-//        q.observe(.value, with: { snapshots in
-//            for snapshot in snapshots.children {
-//                let snap = (snapshot as! DataSnapshot).value as! NSDictionary
-//                results.append(classData(data: snap))
-//            }
-//            completion(results)
-//        })
     }
 
-    public func queryByClass(_ className: String, completion: @escaping (([ClassData]) -> ())) {
+    public func queryByClass(target: String, className: String, completion: @escaping (([ClassData]) -> ())) {
 
-        let q = ref.child("UW")
+        if target != "UW" || target != "userEval" {
+            NSLog("Invalid target")
+            return
+        }
+
+        let q = self.ref.child(target)
                 .queryOrdered(byChild: "class")
-                .queryEqual(toValue: className)
+                .queryStarting(atValue: className)
+                .queryEnding(atValue: className + "\u{F8FF}")
 
         self.query(q, completion: { result in completion(result) })
+    }
 
+    public func uploadEvaluation(map: [String : Any]) {
+        self.ref.child("userEval").setValue(data)
+    }
 
-//        var results: [classData] = []
-//        q.observe(.value, with: { snapshots in
-//            for snapshot in snapshots.children {
-//                let snap = (snapshot as! DataSnapshot).value as! NSDictionary
-//                results.append(classData(data: snap))
-//            }
-//            completion(results)
-//        })
+    public func uploadEvaluation(classData: ClassData) {
+        self.ref.child("userEval").setValue(data.getSelf())
     }
 
     private func query(_ q: DatabaseQuery, completion: @escaping (([ClassData]) -> Void)) {
         var results: [ClassData] = []
         q.observe(.value, with: { snapshots in
+            NSLog("received results")
+            var counter = 0
+
             for snapshot in snapshots.children {
-//                let snap = (snapshot as! DataSnapshot).value as! NSDictionary
+                counter += 1
                 let snap = snapshot as! DataSnapshot
                 results.append(ClassData(snap))
             }
+
+            NSLog("completed of size \(counter)")
             completion(results)
+        })
+    }
+
+    func queryAllClasses() {
+        let limit = 5
+        var counter = 0
+
+        var uniqueClass: Set = [""]
+
+        ref.child("UW").queryOrdered(byChild: "class").observe(.value, with: { snapshots in
+            for snapshot in snapshots.children {
+                counter += 1
+
+                let snap = (snapshot as! DataSnapshot).value as! [String : Any]
+                let className = snap["class"] as! String // format
+
+                if !uniqueClass.contains(className) { uniqueClass.insert(className)}
+                if counter >= limit { break }
+            }
         })
     }
 }
