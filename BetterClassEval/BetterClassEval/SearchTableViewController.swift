15ad03e25b062bb31f17cab98d9e9cf1a781268e
Rico Wang <ricowang@uw.edu>
2018-12-09 13:40:46 -0800

    .

---
+++ BetterClassEval/BetterClassEval/SearchTableViewController.swift
@@ -0,0 +1,36 @@
+//
+//  SearchTableViewController.swift
+//  BetterClassEval
+//
+//  Created by Rico Wang on 12/9/18.
+//  Copyright © 2018 Group_5. All rights reserved.
+//
+
+import UIKit
+
+class SearchTableViewController: UITableViewController, UISearchBarDelegate, UISearchResultsUpdating {
+    func updateSearchResults(for searchController: UISearchController) {
+        <#code#>
+    }
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 0
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return 0
+    }
+
+
+}
